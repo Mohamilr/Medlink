bf8409c7c738ca93d069e9070051c574fd4f2e36
vkguru <vnwoguru@gmail.com>
2020-10-14 23:05:49 +0100

    update on map component issue #10

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -7,26 +7,52 @@ const Map = () => {
 
   let setLatitude = 6.4550575;
   let setLongitude = 3.3941795;
+  let setZoom = 10
 
   const [viewport, setViewport] = useState({
     latitude: setLatitude,
     longitude: setLongitude,
     height: '50vh',
-    width: '100vw',
-    zoom: 10
-  })
+    width: '90vw',
+    zoom: setZoom
+  });
+
+
+  const handleZoomin = () => {
+
+  }
+
+  const handleZoomout = () => {
+
+  }
+
   return (
-    <section className="map">
-      <ReactMapGL 
-        {...viewport} 
-        mapboxApiAccessToken={token}
-        mapStyle="mapbox://styles/vik1/ckg9cpxi205xp19qsc0tw5el5"
-        onViewportChange={viewport => {
-          setViewport(viewport);
-        }}
-      >
-        Map here
-      </ReactMapGL>
+    <section className="map-holder">
+
+      <div className="map">
+
+        <div className="text">
+          <p>Nice to have you here!</p>
+          <h4>See Healthare facilities aroung you</h4>
+        </div>
+
+        <div className="zoom">
+          <div className="zoom-btn">
+            <button className="zoom-btn_clear" onClick={handleZoomin}>+</button>
+            <button className="zoom-btn_clear" onClick={handleZoomout}>-</button>
+          </div>
+        </div>
+        <ReactMapGL 
+          {...viewport} 
+          mapboxApiAccessToken={token}
+          mapStyle="mapbox://styles/vik1/ckg9cpxi205xp19qsc0tw5el5"
+          onViewportChange={viewport => {
+            setViewport(viewport);
+          }}
+        >
+
+        </ReactMapGL>
+      </div>
     </section>
   )
 }
