5c876ebf77b708bcd69a01e8f86286793c95daaa
vkguru <vnwoguru@gmail.com>
2020-10-14 13:18:28 +0100

    ft-map-component

---
+++ src/components/map/map.js
@@ -0,0 +1,34 @@
+import React, {useState} from 'react';
+import ReactMapGL from 'react-map-gl';
+
+const Map = () => {
+
+  const token = "pk.eyJ1IjoidmlrMSIsImEiOiJja2c5YWNqZHowOG5pMnJ2eWJvOGloM3owIn0.07KDZBZ1aTc2xGIBOm26lw";
+
+  let setLatitude = 6.4550575;
+  let setLongitude = 3.3941795;
+
+  const [viewport, setViewport] = useState({
+    latitude: setLatitude,
+    longitude: setLongitude,
+    height: '100vh',
+    width: '100vw',
+    zoom: 10
+  })
+  return (
+    <section className="map">
+      <ReactMapGL 
+        {...viewport} 
+        mapboxApiAccessToken={token}
+        mapStyle="mapbox://styles/vik1/ckg9cpxi205xp19qsc0tw5el5"
+        onViewportChange={viewport => {
+          setViewport(viewport);
+        }}
+      >
+        Map here
+      </ReactMapGL>
+    </section>
+  )
+}
+
+export default Map;
