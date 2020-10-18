c928f638f1ba4043bc9dbd28519cff9f50a19321
Chinyerem Emmanuel <kelvinesthy@gmail.com>
2020-10-18 13:00:44 +0100

    First was a Mistake... changes corrcted.. Task 8 completed

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -3,18 +3,18 @@ import './footer.css'
 
 const Footer = () => {
     return (
-        <div class="down-section">
-            <section class="down-section-icons">
-                <i class="fa fa-instagram"></i>
-                <i class="fa fa-twitter-square"></i>
-                <i class="fa fa-facebook-official"></i>
+        <div className="down-section">
+            <section className="down-section-icons">
+                <i className="fa fa-instagram"></i>
+                <i className="fa fa-twitter-square"></i>
+                <i className="fa fa-facebook-official"></i>
             </section>
-            <section class="container-fluid down-section-footer hidden-xs">
-                <div class="down-main-grid">
-                    <p class="down-grid-1">2020 Medlink Inc. All content posted on this site is the responsibility of the party postig such content, participation on this site by a party does not endorsement of any other party's contents, products or services.
+            <section className="container-fluid down-section-footer hidden-xs">
+                <div className="down-main-grid">
+                    <p className="down-grid-1">2020 Medlink Inc. All content posted on this site is the responsibility of the party postig such content, participation on this site by a party does not endorsement of any other party's contents, products or services.
                      Contents should not be used for medical advice, diagnosis or treatmeant.
                     </p>
-                    <div class="down-grid-2 down-button-info">
+                    <div className="down-grid-2 down-button-info">
                         <a>AD CHOICES</a>
                         <a>PRIVACY</a>
                         <a>COOKIES</a>
@@ -22,8 +22,8 @@ const Footer = () => {
                     </div>
                 </div>
             </section>
-            <section class="container-fluid down-section-footer hidden-lg">
-                <div class="down-grid-2 down-button-info">
+            <section className="container-fluid down-section-footer hidden-lg">
+                <div className="down-grid-2 down-button-info">
                     <a>AD CHOICES</a>
                     <a>PRIVACY</a>
                     <a>COOKIES</a>
