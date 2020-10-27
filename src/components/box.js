58834a87ae64a77e2c3504aca220f982f7e0be9e
mohamilr <ibrahimdamy7@gmail.com>
2020-10-27 21:31:46 +0100

    working here

--- src/components/box.js
+++ src/components/box.js
@@ -1,9 +1,8 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-import { Map, TileLayer, Marker, Popup, Polyline } from "react-leaflet";
 
 const Box = ({ results, lat, lng }) => {
-  const [points, setPoints] = useState([]);
+
   const direction = async (lat, lng, destLat, destLng) => {
     try {
       const response = await fetch(
@@ -13,32 +12,14 @@ const Box = ({ results, lat, lng }) => {
         }
       );
       const data = await response.json();
-      console.log("initial", data);
 
-      //   if (!data.routes) {
-      //       return console.log('not found')
-      //   }
-
-      const lines = []
-      data.routes[0].legs[0].points.map(line => {
-        const arr = Object.values(line)
+      const lines = [];
+      data.routes[0].legs[0].points.map((line) => {
+        const arr = Object.values(line);
         lines.push(arr);
-      })
+      });
 
-      setPoints(lines);
-      console.log('lines', lines);
-
-      // // array to store latitude and longitude
-      // const latAndLng = []
-      // data.results.map((latlng) => {
-      //    return latAndLng.push({
-      //         lat: latlng.position.lat,
-      //         lng: latlng.position.lon,
-      //         name: latlng.poi.name,
-      //         address: latlng.address.freeformAddress,
-      //     })
-      // })
-      // setResults(latAndLng)
+      localStorage.setItem("points", JSON.stringify(lines));
     } catch (e) {
       console.error(e);
     }
@@ -48,31 +29,12 @@ const Box = ({ results, lat, lng }) => {
     <div>
       {results &&
         results.map((data, index) => (
-          //   <NavLink key={index} to={`/details/${data.name}`}>
+            <NavLink key={index} to={`/details/${data.name}`}>
           <p onClick={() => direction(lat, lng, data.lat, data.lng)}>
             {data.name}
           </p>
-          //   </NavLink>
+            </NavLink>
         ))}
-      <Map center={[6.4550575, 3.3941795]} className="mapLayout" zoom={10}>
-        <TileLayer
-          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        {points && points.map((line, index) => (
-            <Polyline key={index} positions={[line]} color={'green'}/>
-        ))}
-        {results &&
-          results.map((data, index) => (
-            <Marker
-              key={index}
-              marker_index={index}
-              position={[data.lat, data.lng]}
-            >
-              <Popup>{`${data.name}, ${data.address}`}</Popup>
-            </Marker>
-          ))}
-      </Map>
     </div>
   );
 };
--- src/pages/details/details.js
+++ src/pages/details/details.js
@@ -1,18 +1,50 @@
-import React, { useState } from 'react';
-import MapRenderer from '../../components/map/map';
-import Footer from '../../components/footer/footer'
-
+import React, { useState } from "react";
+import { Map, TileLayer, Marker, Popup, Polyline } from "react-leaflet";
+import Footer from "../../components/footer/footer";
 
 const Details = () => {
-    const [lat, setLat] = useState(6.4550575);
-    const [lng, setLng] = useState(3.3941795);
+//   const [start, setStart] = useState([]);
+//   const [end, setEnd] = useState([]);
+  const getPoints = localStorage.getItem("points");
+  const points = JSON.parse(getPoints);
+//   if (points !== null) {
+    // setStart(points[0]);
+    // setEnd([points.length - 1]);
+//   }
+const start = points[0];
+const end = points[points.length - 1]
 
-    return (
-        <main>
-            <MapRenderer lat={lat} lng={lng} />
-            <Footer/>
-        </main>
-    )
-}
+  const markers = [
+    {
+      lat: start[0],
+      lng: start[1],
+      status: "Starting point",
+    },
+    {
+      lat: end[0],
+      lng: end[1],
+      status: "Destination",
+    },
+  ];
+
+  return (
+    <main>
+      <Map center={[6.4550575, 3.3941795]} className="mapLayout" zoom={10}>
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Polyline positions={points} color={"red"} weight={5} />
+        {markers &&
+          markers.map((mark, index) => (
+            <Marker key={index} position={[mark.lat, mark.lng]}>
+              <Popup>{`${mark.status}`}</Popup>
+            </Marker>
+          ))}
+      </Map>
+      <Footer />
+    </main>
+  );
+};
 
 export default Details;
